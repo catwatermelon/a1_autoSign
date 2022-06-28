068773db75bf5c5398b23e51ec659ede0e73c87d
catwatermelon <46562607+catwatermelon@users.noreply.github.com>
2022-06-28 14:12:09 +0800

    Update request.js

--- request.js
+++ request.js
@@ -6,8 +6,9 @@ const utils = require("./utils/utils")
 const users = utils.getUsers(env);
 const allRequest = users.map((user) => {
     return new Promise((resolve, reject) => {
-        console.log(user);
         const url = `${API_PREFIX}?userId=${user.userId}&sign=${user.sign}&timestamp=${user.timestamp}`;
+        console.log(user, user.userId, user.sign, user.timestamp);
+        console.log(url);
         axios.get(url).then(res => {
             const content = res.data.success ? '签到成功' : res.data.message;
             console.log(content);
