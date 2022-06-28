dfbd3e070cf71081e74164b2e842bf29b189d18e
catwatermelon <46562607+catwatermelon@users.noreply.github.com>
2022-06-28 15:22:40 +0800

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
 module.exports = {
     getUsers(env) {
+        console.log(typeof env, env)
         return Object.keys(env).filter(item=>Boolean(env[item])).map(secretName => {
             if(secretName.startsWith('COOKIE_')) {
                 return Object.fromEntries(env[secretName].split('&').map(param => param.split('=')));
