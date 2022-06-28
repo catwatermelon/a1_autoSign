ce7a52df3adb5116613c5ca6d3ff5ae09cad4014
catwatermelon <46562607+catwatermelon@users.noreply.github.com>
2022-06-28 14:02:26 +0800

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 module.exports = {
     getUsers(env) {
         const cookies = Object.keys(env).filter(key => key.match(/^COOKIE_([0-9])+$/));
-        return cookies.map(key=>JSON.parse(env[key])).filter(Boolean);
+        return cookies.map(key=>JSON.env[key]).filter(Boolean).map(item=>JSON.parse(item));
     }
 };
