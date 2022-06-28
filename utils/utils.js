e50d1b791af8352eb28cdaf59a50b28e19f43f0b
catwatermelon <46562607+catwatermelon@users.noreply.github.com>
2022-06-28 13:54:09 +0800

    fix: 修复Cookie取值错误的问题

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
     getUsers(env) {
-        return Object.keys(env).filter(key => key.match(/^COOKIE_([0-9])+$/)).map(item=>JSON.parse(item));
+        return Object.keys(env).filter(key => key.match(/^COOKIE_([0-9])+$/)).map(key=>JSON.parse(env[key]));
     }
 };
